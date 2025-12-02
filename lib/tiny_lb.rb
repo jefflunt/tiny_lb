dea744e100537ab21adca94264af79f244d33368
jefflunt <jefflunt@gmail.com>
2025-12-02 10:11:11 -0600

    updated docs

--- lib/tiny_lb.rb
+++ lib/tiny_lb.rb
@@ -5,10 +5,12 @@
 # on a user-provided strategy.
 #
 # use it in the following way:
-#   1. define your services; they should share a common public interface
-#   2. define a strategy; a strategy is any object that responds to a call to #tiny_lb, and returns 
-#   3. assemble the load balancer.
-#   4. use the load balancer as if it were one of your services.
+#   1. define your services (i.e. objects to which the method calls can be
+#     forwarded)
+#   2. define a load balancing strategy: strategies can be any object that
+#     responds to the method `tiny_lb`, and returns an object to which a method
+#     call should be forwarded; an simple example is shown below
+#   3. initialize the TinyLb instance, given the services and balancing strategy
 #
 # usage:
 #   class ProdSvc
@@ -23,14 +25,16 @@
 #     end
 #   end
 #
+#   # a simple percentage-based load balancing strategy
 #   class Rollout
 #     def initialize(percentage:)
 #       @percentage = percentage
 #     end
 #
 #     def tiny_lb(services)
-#       # services are ordered. services[0] is the primary, services[1]
-#       # is the new service being rolled out to.
+#       # services are ordered in this example:
+#       # - services[0] is the primary/production service
+#       # - services[1] is the new/experimental service
 #       rand(1..100) <= @percentage ? services[1] : services[0]
 #     end
 #   end
@@ -42,6 +46,11 @@
 #
 #   10.times do
 #     puts lb.do_work("my task")
+#
+#     # the `lb` object is an intance of TinyLb, and when you call a method on
+#     # it, `method_missing` will be triggered, the load balancing strategy will
+#     # be run to select a downstream service, and then the method call will be
+#     # forwarded to the selected service
 #   end
 #
 #   # output might look like this:
